da692450b6eade33d1cd71719a25ab4bcc19fb54
Johan <johanvargas1212@gmail.com>
2024-11-13 23:05:15 -0500

    Primera Parte y Segunda

---
+++ scr/src/main/Ejercicio8/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import metodos.Ejercicios;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Ejercicios ej1 = new Ejercicios();
+
+        ej1.llenar();
+        ej1.imprimirdatos();
+    }
+}
